f96597bfc89bf329771b9f3549b366e808229f98
ObyeBoss <arssovetnikov@gmail.com>
2018-06-14 19:29:03 +0300

    16bit uhqa fix

--- sound/soc/codecs/wcd9330.c
+++ sound/soc/codecs/wcd9330.c
@@ -712,9 +712,10 @@ static int tomtom_update_uhqa_mode(struct snd_soc_codec *codec, int path)
 	struct tomtom_priv *tomtom_p = snd_soc_codec_get_drvdata(codec);
 
 	
-	if ((tomtom_compare_bit_format(codec, 24)) || ((tomtom_compare_bit_format(codec, 16)) {
+	if ((tomtom_compare_bit_format(codec, 24)) || ((tomtom_compare_bit_format(codec, 16))) {
 		tomtom_p->uhqa_mode = 1;
 	}
+
 	dev_dbg(codec->dev, "%s: uhqa_mode=%d", __func__, tomtom_p->uhqa_mode);
 	return ret;
 }
